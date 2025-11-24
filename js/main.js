abe5cc3262881af478ade9e6a7a57237b949c29d
KimDuHyun-web <duhyeon467@gmail.com>
2025-11-25 08:59:17 +0900

    feat: back to top 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const btt = document.querySelector('#btt');
 const qs = document.querySelectorAll('.question');
 
+window.scrollTo(0, 300);
+
+//백투탑
+window.addEventListener('scroll',()=>{
+  let scrAmt = window.scrollY;
+  if(scrAmt >= 300){
+    btt.classList.add('active');
+  }else{
+    btt.classList.remove('active');
+  }
+});
+btt.addEventListener('click',(e)=>{
+  e.preventDefault();
+  window.scrollTo({
+    left:0,
+    top:0,
+    behavior:'smooth'
+  });
+});
+
 // 아코디언
 for(let q of qs){
   q.addEventListener('click',()=>{
