77e419cc920a869eda417e5d192a76afb9f07b4d
KimDuHyun-web <duhyeon467@gmail.com>
2025-11-25 18:47:04 +0900

    feat: about js 생성 및 내용 수정

---
+++ js/about.js
@@ -0,0 +1,11 @@
+const qs = document.querySelectorAll('.question');
+
+// 아코디언
+for(let q of qs){
+  q.addEventListener('click',()=>{
+    for(let q of qs){
+      q.classList.remove('active');
+    }
+    q.classList.add('active');
+  });
+}
